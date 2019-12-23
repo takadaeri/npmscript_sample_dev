daa1786df496f1c7d5c6e2a5ba473e53598826c6
e-takada <eri.takada@imjp.co.jp>
2019-12-23 10:56:54 +0900

    カードモジュール追加

--- src/_developresources/_js/modules/getBook.js
+++ src/_developresources/_js/modules/getBook.js
@@ -22,18 +22,44 @@ const getBook = () => {
           var ul = document.querySelector('ul');
 
           var clone = document.importNode(template.content, true);
+          var a = clone.querySelector('a');
           var p = clone.querySelector('p');
           var img = clone.querySelector('img');
+          a.href = 'https://books.google.co.jp/books?id=' + response.data.items[0].id;
+          a.target = '_blank';
           p.textContent = response.data.items[0].volumeInfo.title;
           img.src = response.data.items[0].volumeInfo.imageLinks.thumbnail.replace('http', 'https');
           ul.appendChild(clone);
 
           var clone2 = document.importNode(template.content, true);
+          var a2 = clone2.querySelector('a');
           var p2 = clone2.querySelector('p');
           var img2 = clone2.querySelector('img');
+          a2.href = 'https://books.google.co.jp/books?id=' + response.data.items[1].id;
+          a2.target = '_blank';
           p2.textContent = response.data.items[1].volumeInfo.title;
           img2.src = response.data.items[1].volumeInfo.imageLinks.thumbnail.replace('http', 'https');
           ul.appendChild(clone2);
+
+          var clone3 = document.importNode(template.content, true);
+          var a3 = clone3.querySelector('a');
+          var p3 = clone3.querySelector('p');
+          var img3 = clone3.querySelector('img');
+          a3.href = 'https://books.google.co.jp/books?id=' + response.data.items[2].id;
+          a3.target = '_blank';
+          p3.textContent = response.data.items[2].volumeInfo.title;
+          img3.src = response.data.items[2].volumeInfo.imageLinks.thumbnail.replace('http', 'https');
+          ul.appendChild(clone3);
+
+          var clone4 = document.importNode(template.content, true);
+          var a4 = clone4.querySelector('a');
+          var p4 = clone4.querySelector('p');
+          var img4 = clone4.querySelector('img');
+          a4.href = 'https://books.google.co.jp/books?id=' + response.data.items[3].id;
+          a4.target = '_blank';
+          p4.textContent = response.data.items[3].volumeInfo.title;
+          img4.src = response.data.items[3].volumeInfo.imageLinks.thumbnail.replace('http', 'https');
+          ul.appendChild(clone4);
 
 
         })
